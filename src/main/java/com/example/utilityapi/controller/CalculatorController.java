984faf66751134786a7a2f01961f17409ce0d27b
PhilosophyWithJosh <joshmendiola@icloud.com>
2022-09-12 16:36:08 -0500

    Initial commit

---
+++ src/main/java/com/example/utilityapi/controller/CalculatorController.java
@@ -0,0 +1,25 @@
+package com.example.utilityapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CalculatorController {
+
+    public CalculatorController() {
+    }
+
+    @RequestMapping(value = "/calculator/divide", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public double divide(@RequestParam int value1, @RequestParam int value2) {
+
+        return value1 / value2;
+    }
+
+    @RequestMapping(value = "/calculator/square/{value}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public int square(@PathVariable int value) {
+
+        return value * value;
+    }
+}
